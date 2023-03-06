f4dd2064df454953148777715db3d4b1994af337
Areeba T. Shoaib <areebashoaib99@gmail.com>
2023-03-06 10:46:36 +0100

    (How to initialize population) feat Initializing_Population.py

--- Initializing_Population/Initializing_Population/Initializing_Population.py
+++ Initializing_Population/Initializing_Population/Initializing_Population.py
@@ -1,1 +1,19 @@
+import random
+
+# Define the range of values
+MIN_VALUE = -10
+MAX_VALUE = 10
+
+# Define the size of the population
+POPULATION_SIZE = 50
+
+# Create an initial population of chromosomes
+population = []
+for i in range(POPULATION_SIZE):
+    chromosome = random.uniform(MIN_VALUE, MAX_VALUE)
+    population.append(chromosome)
+
+# Print the population
+print("Initial population:")
+print(population)
 
